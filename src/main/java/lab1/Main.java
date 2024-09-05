401aa8309c36a58593f46d884b056dab2088016a
Ivan <gendalf.mesa@gmail.com>
2024-09-05 15:10:04 +0300

    init

---
+++ src/main/java/lab1/Main.java
@@ -0,0 +1,13 @@
+package lab1;
+
+public class Main {
+
+    public static String convertToBinary(int num) {
+        return "Hello";
+    }
+
+    public static void main(String[] args) {
+        convertToBinary(4534);
+        System.out.println("Hello world!");
+    }
+}
